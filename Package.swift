6fa095dc499a4b22888a70867aaa383aeb2d8621
Luciano Almeida <passos.luciano@outlook.com>
2019-03-29 00:43:29 -0300

    Suporting swift folder. TODO: Make it recursive.

--- Package.swift
+++ Package.swift
@@ -13,14 +13,15 @@ let package = Package(
         
         ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0"))
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
+        .package(url: "https://github.com/mxcl/Path.swift.git", .exact("0.16.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "modifier-remover",
-            dependencies: ["modifier-remover-core"],
+            dependencies: ["modifier-remover-core", "Path"],
             path: "Sources/modifier-remover/tool"),
         .target(
             name: "modifier-remover-core",
--- Sources/modifier-remover/tool/main.swift
+++ Sources/modifier-remover/tool/main.swift
@@ -1,18 +1,37 @@
 import SwiftSyntax
 import Foundation
+import Path
+
 import modifier_remover_core
 
-let url = URL(string: "file:/Users/lucianoalmeida/Documents/Programming/OpenProjects/Community/SwifterSwift/Sources/Extensions/SwiftStdlib/DictionaryExtensions.swift")!
+func rewrite(source: Path) throws {
+    let sourceFile = try SyntaxTreeParser.parse(source.url)
+    
+    let rewriter = PublicModifierExtensionRewriter()
+    
+    let result = rewriter.visit(sourceFile)
+    
+    var contents: String = ""
+    result.write(to: &contents)
+    
+    print(contents)
+    
+    //try? contents.write(to: url, atomically: true, encoding: .utf8)
+}
 
-let sourceFile = try SyntaxTreeParser.parse(url)
+guard CommandLine.arguments.count > 1, let path = Path(CommandLine.arguments[1]) else {
+    fatalError("You should provide a folder")
+}
 
-let rewriter = PublicModifierExtensionRewriter()
+var files: [Path] = []
 
-let result = rewriter.visit(sourceFile)
+if path.isFile && path.extension == "swift" {
+    files.append(path)
+} else {
+    files = try path.ls().files(withExtension: "swift")
+}
 
-var contents: String = ""
-result.write(to: &contents)
-
-print(contents)
-
-//try? contents.write(to: url, atomically: true, encoding: .utf8)
+for file in files {
+    print("Rewriting \(file.basename())")
+    try rewrite(source: file)
+}
