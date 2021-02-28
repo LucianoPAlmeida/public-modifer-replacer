a89ac5e0f70a181540b6a6907b1745e8264c3ad1
Luciano Almeida <passos.luciano@outlook.com>
2021-02-28 18:48:36 -0300

    [Update] Updating for 5.3

--- Sources/modifier-remover/core/PublicModifierRewriter.swift
+++ Sources/modifier-remover/core/PublicModifierRewriter.swift
@@ -2,47 +2,46 @@ import Foundation
 import SwiftSyntax
 
 public class PublicModifierExtensionRewriter: SyntaxRewriter {
-
-    private func makeNewLineSpacesModifier(withLeadingTrivia trivia: Trivia?) -> DeclModifierSyntax {
-        guard let trivia = trivia else { return SyntaxFactory.makeBlankDeclModifier() }
-        return SyntaxFactory.makeBlankDeclModifier().withDetail(
-            SyntaxFactory.makeUnknown("").withLeadingTrivia(trivia)
-        )
+  
+  private func makeNewLineSpacesModifier(withLeadingTrivia trivia: Trivia?) -> DeclModifierSyntax {
+    guard let trivia = trivia else { return SyntaxFactory.makeBlankDeclModifier() }
+    return SyntaxFactory.makeBlankDeclModifier().withDetail(
+      SyntaxFactory.makeUnknown("").withLeadingTrivia(trivia)
+    )
+  }
+  
+  public override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
+    return DeclSyntax(replacePublicModifier(node: node))
+  }
+  
+  public override func visit(_ node: VariableDeclSyntax) -> DeclSyntax {
+    return DeclSyntax(replacePublicModifier(node: node))
+  }
+  
+  func replacePublicModifier<M: ModifiersSyntax>(node: M) -> M {
+    guard let modifiers = node.modifiers else { return node }
+    guard let extDecl = searchExtensionDeclParent(node: node), extDecl.isPublicExtension else {
+      debugPrint("Is NOT declared on a public extension, so we don't need to remove public modifier")
+      return node
+    }
+    
+    guard let publicModifier = modifiers.first(where: { $0.name.tokenKind == .publicKeyword }) else {
+      return node
     }
 
-    override public func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
-        return replacePublicModifier(node: node)
+    if publicModifier.indexInParent == 0 {
+      let newLineTabModifier = makeNewLineSpacesModifier(withLeadingTrivia: publicModifier.leadingTrivia)
+      return node.withModifiers(modifiers.replacing(childAt: 0, with: newLineTabModifier))
     }
-
-    public override func visit(_ node: VariableDeclSyntax) -> DeclSyntax {
-        return replacePublicModifier(node: node)
-    }
-
-    func replacePublicModifier<M: ModifiersSyntax>(node: M) -> M {
-        guard let modifiers = node.modifiers else { return node }
-        guard let extDecl = searchExtensionDeclParent(node: node), extDecl.isPublicExtension else {
-            print("Is NOT declared on a public extension, so we don't need to remove")
-            return node
-        }
-        if let publicModifier = modifiers.first(where: { $0.name.tokenKind == .publicKeyword }) {
-            if publicModifier.indexInParent == 0 {
-                let newLineTabModifier = makeNewLineSpacesModifier(withLeadingTrivia: publicModifier.leadingTrivia)
-                return node.withModifiers(modifiers.replacing(childAt: 0, with: newLineTabModifier))
-            } else {
-                return node.withModifiers(modifiers.removing(childAt: publicModifier.indexInParent))
-            }
-        }
-        return node
-    }
-
+    return node.withModifiers(modifiers.removing(childAt: publicModifier.indexInParent))
+  }
 }
 
 // Finding the Extension Decl parent. 
-private func searchExtensionDeclParent(node: Syntax?) -> ExtensionDeclSyntax? {
-    guard let node = node else { return nil }
-    if let extensionDecl = node.parent as? ExtensionDeclSyntax {
-        return extensionDecl
-    } else {
-        return searchExtensionDeclParent(node: node.parent)
-    }
+private func searchExtensionDeclParent(node: SyntaxProtocol) -> ExtensionDeclSyntax? {
+  guard let parent = node.parent else { return nil }
+  if let extensionDecl = parent.as(ExtensionDeclSyntax.self) {
+    return extensionDecl
+  }
+  return searchExtensionDeclParent(node: parent)
 }
--- Tests/modifier-removerTests/modifier_removerTests.swift
+++ Tests/modifier-removerTests/modifier_removerTests.swift
@@ -6,7 +6,7 @@ import modifier_remover_core
 
 final class modifier_removerTests: XCTestCase {
     
-    let testSource = """
+  let testSource = """
         public extension Int {
 
             public func f() -> Int {
@@ -21,7 +21,7 @@ final class modifier_removerTests: XCTestCase {
         }
     """
     
-    let expectedTestSource = """
+  let expectedTestSource = """
         public extension Int {
 
             func f() -> Int {
@@ -36,7 +36,7 @@ final class modifier_removerTests: XCTestCase {
         }
     """
     
-    let testNonPublicSource = """
+  let testNonPublicSource = """
         extension Int {
 
             public func f() -> Int {
@@ -50,44 +50,33 @@ final class modifier_removerTests: XCTestCase {
             }
         }
     """
+  
+  func testSubstitutionPublicExtension() throws {
+    let sourceFile = try SyntaxParser.parse(source: testSource)
     
-    let testFileName: String = "TestFile.swift"
-    let testFileNameNonPublic: String = "TestFile-NonPublic.swift"
+    let publicRewriter = PublicModifierExtensionRewriter()
+    let result = publicRewriter.visit(sourceFile)
     
-    override func setUp() {
-        // Work around https://bugs.swift.org/browse/SR-2866
-        try? testSource.write(to: Bundle(for: type(of: self)).bundleURL.appendingPathComponent(testFileName), atomically: true, encoding: .utf8)
-        try? testNonPublicSource.write(to: Bundle(for: type(of: self)).bundleURL.appendingPathComponent(testFileNameNonPublic), atomically: true, encoding: .utf8)
-    }
+    var contents: String = ""
+    result.write(to: &contents)
     
-    func testSubstitutionPublicExtension() throws {
-        let url = Bundle(for: type(of: self)).bundleURL.appendingPathComponent(testFileName)
-        let sourceFile = try SyntaxParser.parse(url)
-        
-        let publicRewriter = PublicModifierExtensionRewriter()
-        let result = publicRewriter.visit(sourceFile)
-        
-        var contents: String = ""
-        result.write(to: &contents)
-        
-        XCTAssertEqual(expectedTestSource, contents)
-    }
+    XCTAssertEqual(expectedTestSource, contents)
+  }
+  
+  func testSubstitutionPublicNonExtension() throws {
+    let sourceFile = try SyntaxParser.parse(source: testNonPublicSource)
     
-    func testSubstitutionPublicNonExtension() throws {
-        let url = Bundle(for: type(of: self)).bundleURL.appendingPathComponent(testFileNameNonPublic)
-        let sourceFile = try SyntaxParser.parse(url)
-        
-        let publicRewriter = PublicModifierExtensionRewriter()
-        let result = publicRewriter.visit(sourceFile)
-        
-        var contents: String = ""
-        result.write(to: &contents)
-        print(contents)
-        XCTAssertEqual(testNonPublicSource, contents)
-    }
+    let publicRewriter = PublicModifierExtensionRewriter()
+    let result = publicRewriter.visit(sourceFile)
     
-    static var allTests = [
-        ("testSubstitutionPublicExtension", testSubstitutionPublicExtension),
-        ("testSubstitutionPublicNonExtension", testSubstitutionPublicNonExtension),
-    ]
+    var contents: String = ""
+    result.write(to: &contents)
+    
+    XCTAssertEqual(testNonPublicSource, contents)
+  }
+  
+  static var allTests = [
+    ("testSubstitutionPublicExtension", testSubstitutionPublicExtension),
+    ("testSubstitutionPublicNonExtension", testSubstitutionPublicNonExtension),
+  ]
 }
